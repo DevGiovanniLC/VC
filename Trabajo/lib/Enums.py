90d2cc1318e684798beee03f8de53a95305586dd
Giovanni León <giovannilc.dev@gmail.com>
2025-01-07 11:38:22 +0000

    trabajo: Importación de los enums

---
+++ Trabajo/lib/Enums.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class EnumDataLoader (Enum):
+	INTELLIGENCE = 0
+	SIZE = 1
+	WEIGHT = 2
+
+
+class EnumDogList(Enum):
+	FRAME = 0
+	ACCURACY = 1
+	COLOR = 2
+	BREED = 3
+	EMOTION = 4
+	COUNT = 5
+
+
